0652ee7687ea1721ee4bdbbefefb3c7fe95c265b
Nuriddin <inoyatovnuriddin009@gmail.com>
2023-09-20 15:32:38 +0500

    some fixes

--- src/main/java/uz/fastfood/dashboard/service/impl/OrderServiceImpl.java
+++ src/main/java/uz/fastfood/dashboard/service/impl/OrderServiceImpl.java
@@ -2,6 +2,8 @@ package uz.fastfood.dashboard.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.fastfood.dashboard.dto.request.OrderRequest;
+import uz.fastfood.dashboard.dto.response.BaseResponse;
 import uz.fastfood.dashboard.service.OrderService;
 
 @Service
@@ -9,6 +11,8 @@ import uz.fastfood.dashboard.service.OrderService;
 public class OrderServiceImpl implements OrderService {
 
 
-
-
+    @Override
+    public BaseResponse<?> makeOrder(OrderRequest request, BaseResponse<?> response) {
+        return null;
+    }
 }
